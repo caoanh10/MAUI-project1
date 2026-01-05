fbbc0b0a435441332b37d66d7943d22a8328c4f8
caoanh10 <lecaoanh101205@gmail.com>
2026-01-05 10:36:28 +0700

    add Nam

--- SmartHomeDashboard/AppShell.xaml.cs
+++ SmartHomeDashboard/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace SmartHomeDashboard
+﻿namespace SmartHomeDashboard;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
